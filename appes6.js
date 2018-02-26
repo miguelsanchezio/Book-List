339feffa0738855c13620fd66f663a51ef397c00
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-25 21:08:25 -0800

    Added ability to use local storage to add and remove books

--- appes6.js
+++ appes6.js
@@ -54,6 +54,54 @@ class UI {
     }
 }
 
+// Local storage class
+class Store {
+    static getBooks() {
+        let books;
+
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function(book) {
+            const ui = new UI;
+
+            // Add book to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+        
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index) {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event listener for adding books
 document.getElementById('book-form').addEventListener('submit', function(e) {
     const title = document.getElementById('title').value,
@@ -73,6 +121,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
         // Add book to list
         ui.addBookToList(book);
 
+        // Add to local storage
+        Store.addBook(book);
+
         // Show success
         ui.showAlert(`${title} has been added.`, 'success');
         
@@ -90,6 +141,9 @@ document.getElementById('book-list').addEventListener('click', function(e) {
 
     // Delete book
     ui.deleteBook(e.target);
+
+    // Remove from local storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show message
     ui.showAlert('Book Removed!', 'success');
